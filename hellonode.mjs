5e9423458fd22c9664b6dc1aa977bed6b6eb324b
Abhinav Shrivastava <dev@abhinav.tech>
2018-08-29 17:31:19 +0530

    ES6 modules added

---
+++ hellonode.mjs
@@ -0,0 +1,93 @@
+import singleData from './single_data_file';
+
+var allData = singleData.allData;
+
+//Load HTTP module
+import http from 'http';
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const stripUnusedFields = hero => {
+  // All the fields mentioned here are just the ones to keep.
+  const { id, name, image, powerstats } = hero;
+  return { id, name, image, powerstats };
+};
+
+//Create HTTP server and listen on port 3000 for requests
+const server = http.createServer((req, res) => {
+  let responseContent = "";
+  console.log("req url is: ", req.url);
+  //Set the response HTTP header with HTTP status and Content type
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+
+  if (req.url.startsWith("/all")) {
+    responseContent = handleReqAll(req.url);
+    res.end(JSON.stringify(responseContent));
+  } else if (req.url == "/favicon.ico") {
+    /* no favicons */
+  } else {
+    handleSuperheroApi(req.url).then(responseContent =>
+      res.end(JSON.stringify(stripUnusedFields(responseContent)))
+    );
+  }
+});
+
+var handleSuperheroApi = async function(url) {
+  const options = {
+    host: "superheroapi.com",
+    path: url.replace("/api", "/api.php"),
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+  const externalCallPromise = new Promise((resolve, reject) =>
+    http.get(options, res => {
+      let rawData = "";
+      res.on("data", chunk => {
+        rawData += chunk;
+      });
+      res.on("end", () => {
+        try {
+          const parsedData = JSON.parse(rawData);
+          resolve(parsedData);
+        } catch (e) {
+          console.error(e.message);
+          reject(e);
+        }
+      });
+    })
+  );
+  const reply = await externalCallPromise;
+  return reply;
+};
+
+var handleReqAll = function(url) {
+  let max = getMax(url);
+  return {
+    superheroes: allData.slice(0, max).map(stripUnusedFields)
+  };
+};
+
+var getMax = function(url) {
+  const regex1 = /max=([0-9]*)/;
+  const match = regex1.exec(url);
+  if (match != null) {
+    return match[1];
+  }
+};
+
+//listen for request on port 3000, and as a callback function have the port listened on logged
+server.listen(port, hostname, () => {
+  console.log(`Server running
+  Useage:
+  1. To get a list of all superheroes:
+  http://${hostname}:${port}/all
+
+  2. To get a limited number of them:
+  http://${hostname}:${port}/all?max=10
+
+  3. To get a hero by their id:
+  http://${hostname}:${port}/<your access token>/1`);
+});
