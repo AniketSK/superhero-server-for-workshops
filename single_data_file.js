3eb1e7fb91796072bab1cee3817978a1fa665e2e
Aniket Kadam <anikadamg@gmail.com>
2018-08-30 13:43:39 +0530

    Added info for Shuri

--- single_data_file.js
+++ single_data_file.js
@@ -1,6 +1,48 @@
 const allData = [
   {
     response: "success",
+    id: "732",
+    name: "Shuri",
+    powerstats: {
+      intelligence: "100",
+      strength: "14",
+      speed: "30",
+      durability: "9",
+      power: "25",
+      combat: "70"
+    },
+    biography: {
+      "full-name": "Shrui",
+      "alter-egos": "Black Panther",
+      aliases: ["Shuri, Griot"],
+      "place-of-birth": "Wakanda",
+      "first-appearance": "Black Panther #2 (May, 2005)",
+      publisher: "Marvel Comics",
+      alignment: "good"
+    },
+    appearance: {
+      gender: "Female",
+      race: "Human",
+      height: ["5'9", "175 cm"],
+      weight: ["150 lb", "68 kg"],
+      "eye-color": "Brown",
+      "hair-color": "Black"
+    },
+    work: {
+      occupation:
+        "Head of the Science and Information Exchange.",
+      base: "Wakanda"
+    },
+    connections: {
+      "group-affiliation": "Wakandan Design Group",
+      relatives: "T'Challa (brother), Bashenga (paternal ancestor, deceased), Azzuri the Wise (paternal grandfather, deceased), Nanali (paternal grandmother, deceased), Chanda (paternal grandfather, presumably deceased), T?Chaka (father, deceased), S'Yan (uncle, deceased), N?Yami (mother, deceased), Ramonda (stepmother), Hunter (adopted brother), Jakarra (half-brother)"
+    },
+    image: {
+      url: "https://image.ibb.co/hFrbCp/shuri.jpg"
+    }
+  },
+  {
+    response: "success",
     id: "1",
     name: "A-Bomb",
     powerstats: {
